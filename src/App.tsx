8175dc4d634c69037fd11ccb33c75bf98ed61e31
joey <jotosound@gmail.com>
2023-04-16 20:52:09 -0400

    update

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
         </a>
         
       </motion.div>
-      <h1>Howdy</h1>
+      <h1>Howdy </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://jotosound.github.io/
 export default defineConfig({
-  base: '/jotosound.github.io/',
+  base: '/',
   plugins: [react()],
 })
