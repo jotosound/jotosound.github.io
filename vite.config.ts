14d323aebff40bafebc4636602a3392d8aaeb2ac
joey <jotosound@gmail.com>
2023-04-16 20:29:07 -0400

    config update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://jotosound.github.io/
 export default defineConfig({
   base: '/jotosound.github.io/',
   plugins: [react()],
